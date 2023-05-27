4a430738c18340d89e4b2af5029c4a0b60e3a01f
CaptainFlooyd <marquesgggamer@gmail.com>
2023-05-27 17:28:38 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let frase ="O rato roeu a roupa do rei de roma";
+
+let palavra = frase.split(" ");
+console.log(palavra)
+
+for(let i = 0; i <= palavra.length; i++){
+     console.log(palavra[i])
+}
